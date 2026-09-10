3d70b3f6150d81ec5aeeb4d090de7c124294bd43
PRIYANSHU JHA <priyanshu.j.2807@gmail.com>
2026-09-10 07:46:13 +0530

    Create 07_led_fade_for_loop.ino

---
+++ 07_led_fade_for_loop.ino
@@ -0,0 +1,17 @@
+const int ledPin = 9;
+
+void setup() {
+  pinMode(ledPin, OUTPUT);
+}
+
+void loop() {
+  for (int brightness = 0; brightness <= 255; brightness++) {
+    analogWrite(ledPin, brightness);
+    delay(5);
+  }
+
+  for (int brightness = 255; brightness >= 0; brightness--) {
+    analogWrite(ledPin, brightness);
+    delay(5);
+  }
+}
